e42aff81b2fa086d84dabe43ac7f48c65f6d031b
Abraham Quintana <93547082+AbrahamQM@users.noreply.github.com>
2022-05-09 21:52:23 +0100

    Organizo mejor

---
+++ 1.Introducción a JavaScript/sesion4_Ejecución-y-comentarios/index.js
@@ -0,0 +1,14 @@
+console.log("Este es el punto de entrada");
+
+//esto es un ejemplo de comentario.
+
+//Este va a ser el mensaje que se envía para despedir
+console.log("Adiós")
+
+/* 
+    Este 
+    es 
+    un comentario
+    de varias
+    líneas
+*/
